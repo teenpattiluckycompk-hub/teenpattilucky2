92e83f37760b81a459c07083c1f3f9f60418edd9
teenpattiluckycompk-hub <teenpattilucky.com.pk@gmail.com>
2026-03-23 23:58:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("3 Patti Go Game Page Loaded Successfully");
+
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", function() {
+        alert("Redirecting to Official 3 Patti Go Download Page!");
+    });
+
+});
